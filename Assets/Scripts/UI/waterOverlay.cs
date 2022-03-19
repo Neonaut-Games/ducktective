79bb28d094251785be0fe9606ba328812ae296a5
Cooper Sullivan <thecoopersully@gmail.com>
2022-03-19 16:43:48 -0500

    Removed WaterOverlay & fixed object persistency

--- Assets/Scripts/UI/waterOverlay.cs
+++ Assets/Scripts/UI/waterOverlay.cs
@@ -6,19 +6,15 @@ namespace UI
 {
     public class WaterOverlay : MonoBehaviour
     {
-        public GameObject overlay;
-
         private void OnTriggerEnter(Collider other)
         {
             if (!other.CompareTag("Player")) return;
-            overlay.SetActive(true);
             StartCoroutine(DrownTick());
         }
 
         private void OnTriggerExit(Collider other)
         {
             if (!other.CompareTag("Player")) return;
-            overlay.SetActive(false);
             StopAllCoroutines();
         }
 
