512c3704596a50ae3cda34300b8fabec5dfe9293
Cooper Sullivan <thecoopersully@gmail.com>
2022-03-31 13:22:40 -0500

    Added WickedMouse font & updated mainMenu
    - Added sounds to main menu
    - Fixed button glitch w/ selection in main menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -3,22 +3,32 @@ using UnityEngine.SceneManagement;
 
 namespace UI
 {
+    [RequireComponent(typeof(AudioSource))]
     public class MainMenu : MonoBehaviour
     {
-
+        public AudioSource clickSound;
+        
         public void StartGame()
         {
+            ButtonClick();
             SceneManager.LoadScene(1);
         }
 
         public void About()
         {
+            ButtonClick();
             Application.OpenURL("https://coopersully.me/projects/ducktective/");
         }
     
         public void QuitGame()
         {
+            ButtonClick();
             Application.Quit();
+        }
+
+        private void ButtonClick()
+        {
+            clickSound.Play();
         }
 
     }
